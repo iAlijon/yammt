c66d3d39690fdde16535168f0fcd85660f91b16c
iAlijon <Alijon.Sh098@gmail.com>
2023-11-24 16:38:06 +0500

    last

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -8,42 +8,62 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Document</title>
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+{{--    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">--}}
 
+{{--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">--}}
+{{--    <!-- Font Awesome -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">--}}
+{{--    <!-- Tempusdominus Bootstrap 4 -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">--}}
+{{--    <!-- iCheck -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">--}}
+{{--    <!-- overlayScrollbars -->--}}
+
+{{--    <!-- select2 -->--}}
+{{--    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2/css/select2.min.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">--}}
+
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">--}}
+{{--    <!-- Daterange picker -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/daterangepicker/daterangepicker.css')}}">--}}
+{{--    <!-- Toastr -->--}}
+{{--    <link href="{{asset('adminlte/plugins/toastr/toastr.css')}}" rel="stylesheet">--}}
+{{--    <!-- Bootstrap Color Picker -->--}}
+{{--    <link rel="stylesheet" href="{{ asset('adminlte/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}">--}}
+{{--    <!-- summernote -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/plugins/summernote/summernote-bs4.min.css')}}">--}}
+{{--    <!-- Theme style -->--}}
+{{--    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">--}}
+<!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet" href="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/jqvmap/jqvmap.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
     <!-- overlayScrollbars -->
-
-    <!-- select2 -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
-
     <link rel="stylesheet" href="{{asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="{{asset('adminlte/plugins/daterangepicker/daterangepicker.css')}}">
-    <!-- Toastr -->
-    <link href="{{asset('adminlte/plugins/toastr/toastr.css')}}" rel="stylesheet">
-    <!-- Bootstrap Color Picker -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}">
     <!-- summernote -->
     <link rel="stylesheet" href="{{asset('adminlte/plugins/summernote/summernote-bs4.min.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
 </head>
 <body>
-
+<div class="wrapper">
     <!-- topMenu -->
-    @include('admin.layouts.topMenu')
-    <!-- /.topMenu -->
+@include('admin.layouts.topMenu')
+<!-- /.topMenu -->
 
     <!-- leftMenu -->
-    @include('admin.layouts.leftMenu')
-    <!-- /.leftMenu -->
+@include('admin.layouts.leftMenu')
+<!-- /.leftMenu -->
 
     <div class="content-wrapper">
         <!-- Main Content -->
@@ -52,15 +72,55 @@
     </div>
 
     <!-- Control Sidebar -->
-    @include('admin.layouts.rightMenu')
-    <!-- /.Control Sidebar -->
+@include('admin.layouts.rightMenu')
+<!-- /.Control Sidebar -->
 
     <!-- Main Footer -->
-    @include('admin.layouts.footer')
-    <!-- /.Main Footer -->
+@include('admin.layouts.footer')
+<!-- /.Main Footer -->
+</div>
 
 
+{{--    <script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>--}}
+{{--    <!-- Bootstrap 4 -->--}}
+{{--    <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>--}}
+{{--    <!-- ChartJS -->--}}
+{{--    <script src="{{asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>--}}
+{{--    <!-- Sparkline -->--}}
+{{--    <script src="{{asset('adminlte/plugins/sparklines/sparkline.js')}}"></script>--}}
+{{--    <!-- JQVMap -->--}}
+{{--    <script src="{{asset('adminlte/plugins/jqvmap/jquery.vmap.min.js')}}"></script>--}}
+{{--    <script src="{{asset('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>--}}
+{{--    <!-- jQuery Knob Chart -->--}}
+{{--    <script src="{{asset('adminlte/plugins/jquery-knob/jquery.knob.min.js')}}"></script>--}}
+{{--    <!-- daterangepicker -->--}}
+{{--    <script src="{{asset("adminlte/plugins/moment/moment.min.js")}}"></script>--}}
+{{--    <script src="{{asset('adminlte/plugins/daterangepicker/daterangepicker.js')}}"></script>--}}
+{{--    <!-- Tempusdominus Bootstrap 4 -->--}}
+{{--    <script src="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>--}}
+{{--    <!-- Summernote -->--}}
+{{--    <script src="{{asset('adminlte/plugins/summernote/summernote-bs4.min.js')}}"></script>--}}
+{{--    <!-- Combodate -->--}}
+{{--    <script src="{{asset('admin-assets/js/combodate.js')}}"></script>--}}
+{{--    <!-- Combodate -->--}}
+{{--    <!-- Toastr -->--}}
+{{--    <script src="{{asset('adminlte/plugins/toastr/toastr.min.js')}}"></script>--}}
+{{--    <!-- Moment -->--}}
+{{--    <script src="{{asset('admin-assets/js/moment.min.js')}}"></script>--}}
+{{--    <!-- Moment -->--}}
+{{--    <!-- overlayScrollbars -->--}}
+{{--    <script src="{{asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>--}}
+{{--    <!-- AdminLTE App -->--}}
+{{--    <script src="{{asset('adminlte/dist/js/adminlte.js')}}"></script>--}}
+
+    <!-- jQuery -->
     <script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{asset('adminlte/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+{{--    <script>--}}
+{{--        $.widget.bridge('uibutton', $.ui.button)--}}
+{{--    </script>--}}
     <!-- Bootstrap 4 -->
     <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- ChartJS -->
@@ -73,25 +133,23 @@
     <!-- jQuery Knob Chart -->
     <script src="{{asset('adminlte/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
     <!-- daterangepicker -->
-    <script src="{{asset("adminlte/plugins/moment/moment.min.js")}}"></script>
+    <script src="{{asset('adminlte/plugins/moment/moment.min.js')}}"></script>
     <script src="{{asset('adminlte/plugins/daterangepicker/daterangepicker.js')}}"></script>
     <!-- Tempusdominus Bootstrap 4 -->
     <script src="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
     <!-- Summernote -->
     <script src="{{asset('adminlte/plugins/summernote/summernote-bs4.min.js')}}"></script>
-    <!-- Combodate -->
-    <script src="{{asset('admin-assets/js/combodate.js')}}"></script>
-    <!-- Combodate -->
-    <!-- Toastr -->
-    <script src="{{asset('adminlte/plugins/toastr/toastr.min.js')}}"></script>
-    <!-- Moment -->
-    <script src="{{asset('admin-assets/js/moment.min.js')}}"></script>
-    <!-- Moment -->
     <!-- overlayScrollbars -->
     <script src="{{asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
     <!-- AdminLTE App -->
     <script src="{{asset('adminlte/dist/js/adminlte.js')}}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{asset('adminlte/dist/js/demo.js')}}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{asset('adminlte/dist/js/pages/dashboard.js')}}"></script>
+
     <script src="<?= asset('admin-assets/js/add.js').'?v='.time() ?>"></script>
+
     @stack('scripts')
 </body>
 </html>
--- resources/views/Admin/layouts/leftMenu.blade.php
+++ resources/views/Admin/layouts/leftMenu.blade.php
@@ -1,35 +1,75 @@
+{{--<aside class="main-sidebar sidebar-dark-primary elevation-4">--}}
+{{--    <!-- Brand Logo -->--}}
+{{--    <a href="index3.html" class="brand-link">--}}
+{{--        <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">--}}
+{{--        <span class="brand-text font-weight-light">YAMMT</span>--}}
+{{--    </a>--}}
+
+{{--    <!-- Sidebar -->--}}
+{{--    <div class="sidebar">--}}
+{{--        <!-- Sidebar Menu -->--}}
+{{--        <nav class="mt-2">--}}
+{{--            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">--}}
+{{--                <!-- Add icons to the links using the .nav-icon class--}}
+{{--                     with font-awesome or any other icon font library -->--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/admin" class="nav-link {{(request()->is('/admin')) ? 'active' : ''}}">--}}
+{{--                        <i class="nav-icon fas fa-tachometer-alt"></i>--}}
+{{--                        <p>Bosh sahifa</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item">--}}
+{{--                    <a href="{{route('news.index')}}" class="nav-link {{(request()->is('news*')) ? 'active' : ''}}">--}}
+{{--                        <i class="fas fa-file nav-icon"></i>--}}
+{{--                        Yangiliklar--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item">--}}
+{{--                    <a href="#" class="nav-link">--}}
+{{--                        <i class="nav-icon fa fa-solid  fa-bars"></i>--}}
+{{--                        <i class="nav-icon fas fa-copy"></i>--}}
+{{--                        <p>--}}
+{{--                            Menu--}}
+{{--                        </p>--}}
+{{--                        <i class="fas fa-angle-left right"></i>--}}
+{{--                    </a>--}}
+{{--                    <ul class="nav nav-treeview">--}}
+{{--                        <li class="nav-item">--}}
+{{--                            <a href="{{route('menu.index')}}" class="nav-link {{(request()->is('menu*')) ? 'active' : ''}}"><i class="fas fa-file nav-icon"></i> Menu</a>--}}
+{{--                        </li>--}}
+{{--                        <li class="nav-item">--}}
+{{--                            <a href="{{route('menu-item.index')}}" class="nav-link {{request()->is('menu-item*') ? 'active' : ''}}"><i class="fas fa-file nav-icon"></i>Menu Item</a>--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </nav>--}}
+{{--        <!-- /.sidebar-menu -->--}}
+{{--    </div>--}}
+{{--    <!-- /.sidebar -->--}}
+{{--</aside>--}}
+
+<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">YAMMT</span>
+        <span class="brand-text font-weight-light">Yammt</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
+
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="/admin" class="nav-link {{(request()->is('/admin')) ? 'active' : ''}}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Bosh sahifa</p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{route('news.index')}}" class="nav-link {{(request()->is('news*')) ? 'active' : ''}}">
-                        <i class="fas fa-file nav-icon"></i>
-                        Yangiliklar
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-{{--                        <i class="nav-icon fa fa-solid  fa-bars"></i>--}}
-                        <i class="nav-icon fas fa-copy"></i>
+                    <a href="#" class="nav-link {{request()->is('admin/menu*')?'active':''}}">
+                        <i class="nav-icon fas fa-file"></i>
                         <p>
                             Menu
                         </p>
@@ -37,12 +77,26 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{route('menu.index')}}" class="nav-link {{(request()->is('menu*')) ? 'active' : ''}}"><i class="fas fa-file nav-icon"></i> Menu</a>
+                            <a href="{{route('menu.index')}}" class="nav-link {{request()->is('admin/menu*')?'active':''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Menu</p>
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{route('menu-item.index')}}" class="nav-link {{request()->is('menu-item*') ? 'active' : ''}}"><i class="fas fa-file nav-icon"></i>Menu Item</a>
+                            <a href="{{route('menu-item.index')}}" class="nav-link {{request()->is('admin/menu-item*')?'active':''}}">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>MenuItem</p>
+                            </a>
                         </li>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('news.index')}}" class="nav-link {{request()->is('admin/news*')?'active':''}}">
+                        <i class="nav-icon fas fa-file"></i>
+                        <p>
+                            Yangiliklar
+                        </p>
+                    </a>
                 </li>
             </ul>
         </nav>
